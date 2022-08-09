1181b25307d8db8617d4bdfc0d8800fdbfbefca1
UmidulloMahmudov <umidullomahmudov1997@gmail.com>
2022-08-09 12:59:49 +0500

    next project

---
+++ src/App.js
@@ -0,0 +1,189 @@
+import logo from './logo.svg';
+import './App.css';
+import umid from"./olim/karzinka.png"
+function App() {
+  return (
+    <div >
+<header>
+    <div className="d-flex justify-content-between container-fluid">
+      <div className="malik d-flex">
+          <h5>Start bootstrap</h5>
+          <div className="  headleft">
+
+              <p >home</p>
+              <p >about</p>
+              <p >Shop</p>
+      </div>
+
+        </div>
+
+
+        <div className="headright  d-flex justify-content-end">
+            <div className="btn  btn-outline-dark d-flex ">
+                <img src={umid} alt=""/>
+                <p className="p1p1">Cart</p>
+
+                <p className="p2p2">0</p>
+            </div>
+        </div>
+    </div>
+</header>
+<div className="section2">
+    <h1>shop in style</h1>
+    <p>With this shop hompeage template</p>
+</div>
+        <div className="section3  ">
+         <div className="row">
+            <div className="col-10">
+                <div className="row">
+                    <div className="col-3">
+                        <div className="card">
+                            <div className="card-header">
+                                <h6>Sale</h6>
+                                <p>450x300</p>
+                            </div>
+                            <div className="card-body">
+                                <h4>Special Item</h4>
+                                <div className="stars">&starf;  </div>
+                                <div className="sum"><del>$20.000</del><p>-$18.00</p></div>
+
+                                <div className="btn btn-outline-dark"><>View options</></div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-3">
+                        <div className="card">
+                            <div className="card-header">
+                                <h6>Sale</h6>
+                                <p>450x300</p>
+                            </div>
+                            <div className="card-body">
+                                <h4>Special Item</h4>
+                                <div className="stars">&starf;  </div>
+                                <div className="sum"><del>$20.000</del><p>-$18.00</p></div>
+
+                                <div className="btn btn-outline-dark"><>View options</></div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-3">
+                        <div className="card">
+                            <div className="card-header">
+<h6>Sale</h6>
+                                <p>450x300</p>
+                            </div>
+                            <div className="card-body">
+                                <h4>Special Item</h4>
+                                <div className="stars">&starf;  </div>
+                                <div className="sum"><del>$20.000</del><p>-$18.00</p></div>
+
+                                <div className="btn btn-outline-dark"><>View options</></div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-3">
+                        <div className="card">
+                            <div className="card-header">
+                                <h6>Sale</h6>
+                                <p>450x300</p>
+                            </div>
+                            <div className="card-body">
+                                <h4>Special Item</h4>
+                                <div className="stars">&starf;  </div>
+                                <div className="sum"><del>$20.000</del><p>-$18.00</p></div>
+
+                                <div className="btn btn-outline-dark"><>View options</></div>
+
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+                <div className="row">
+                    <div className="col-3">
+                        <div className="card">
+                            <div className="card-header">
+                                <h6>Sale</h6>
+                                <p>450x300</p>
+                            </div>
+                            <div className="card-body">
+                                <h4>Special Item</h4>
+                                <div className="stars">&starf;  </div>
+                                <div className="sum"><del>$20.000</del><p>-$18.00</p></div>
+
+                                <div className="btn btn-outline-dark"><>View options</></div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-3">
+                        <div className="card">
+                            <div className="card-header">
+                                <h6>Sale</h6>
+                                <p>450x300</p>
+                            </div>
+                            <div className="card-body">
+                                <h4>Special Item</h4>
+                                <div className="stars">&starf;  </div>
+                                <div className="sum"><del>$20.000</del><p>-$18.00</p></div>
+
+                                <div className="btn btn-outline-dark"><>View options</></div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-3">
+                        <div className="card">
+                            <div className="card-header">
+                                <h6>Sale</h6>
+                                <p>450x300</p>
+                            </div>
+                            <div className="card-body">
+                                <h4>Special Item</h4>
+                                <div className="stars">&starf;  </div>
+                                <div className="sum"><del>$20.000</del><p>-$18.00</p></div>
+
+                                <div className="btn btn-outline-dark"><>View options</></div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-3">
+                        <div className="card">
+                            <div className="card-header">
+                                <h6>Sale</h6>
+                                <p>450x300</p>
+                            </div>
+                            <div className="card-body">
+                                <h4>Special Item</h4>
+                                <div className="stars">&starf;  </div>
+                                <div className="sum"><del>$20.000</del><p>-$18.00</p></div>
+
+                                <div className="btn btn-outline-dark"><>View options</></div>
+
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+         </div>
+        </div>
+        <div className="section-4 d-flex">
+           <div className="last">
+               <p>Copyright </p>
+               <div className="circle">c</div>
+               <p>Your website 2022</p>
+           </div>
+        </div>
+
+    </div>
+  );
+}
+
+export default App;
